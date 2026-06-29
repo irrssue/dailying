d8ce02f6d730c22ad68e25400bfb4a7645a2434d
irrssue <liam@irrssue.com>
2026-06-29 04:29:58 -0400

    Initial Commit

---
+++ dailying/dailyingApp.swift
@@ -0,0 +1,17 @@
+//
+//  dailyingApp.swift
+//  dailying
+//
+//  Created by Liam Rolert on 6/29/26.
+//
+
+import SwiftUI
+
+@main
+struct dailyingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
